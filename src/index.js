962cefd46a899f38d0cbc8e6c5f64f6452766d8d
vinhtuongluong <vinhtuongluong@gmail.com>
2025-03-22 13:32:35 +0700

    Refactor code: create routes, app/controllers folders

--- src/index.js
+++ src/index.js
@@ -5,10 +5,15 @@ const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
 
+const route = require('./routes')
+
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
 // HTTP logger
-app.use(morgan('combined'))
+//app.use(morgan('combined'))
 
 // Template engine
 app.engine(
@@ -20,13 +25,8 @@ app.engine(
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'resources/views'))
 
-app.get('/', (req, res) => {
-  res.render('home')
-})
-
-app.get('/news', (req, res) => {
-  res.render('news')
-})
+// Routes init
+route(app)
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
